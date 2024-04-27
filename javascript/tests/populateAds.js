8840d8984d875b3bf8bb3c91db0defb55c85f7c5
LeulMezgebe <leulgebregmt@gmail.com>
2024-04-27 11:27:19 -0400

    fb_ad population

--- javascript/tests/populateAds.js
+++ javascript/tests/populateAds.js
@@ -2,6 +2,7 @@ const { Client } = require('pg');
 const axios = require('axios');
 
 
+<<<<<<< Updated upstream
 const dbOptions = {
     user: 'postgres',
     host: 'omnirds.cluster-chcpmc0xmfre.us-east-2.rds.amazonaws.com',
@@ -25,6 +26,40 @@ async function getAdsets () {
     
     
     const response = await axios.get(apiUrl, {
+=======
+
+
+// AWS RDS POSTGRESQL INSTANCE
+const dbOptions = {
+  user: 'postgres',
+  host: 'omnirds.cluster-chcpmc0xmfre.us-east-2.rds.amazonaws.com',
+  database: 'postgres',
+  password: 'Omni2023!',
+  port: '5432',
+};
+
+// Create a new PostgreSQL client
+const client = new Client(dbOptions);
+
+// Connect to the PostgreSQL database
+client.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
+
+
+
+
+  
+
+
+  async function getCampaigns () {
+    try {
+      const apiUrl = 'https://graph.facebook.com/v19.0/act_331027669725413';
+      const fields = 'ads{account_id,ad_active_time,ad_review_feedback,ad_schedule_end_time,ad_schedule_start_time,adset_id,campaign_id,configured_status,conversion_domain,created_time,creative,id,effective_status,bid_amount,last_updated_by_app_id,name,preview_shareable_link,recommendations,source_ad,source_ad_id,status,tracking_specs,updated_time,adcreatives{id}}';
+      const accessToken = 'EAAMJLvHGvzkBO7XFdUP87UehMCTSH0JUpxbBUNmmddMLrPx18ltodOEXa9DXZBASRlRQJVQPpRZBfRtyZAzZCoONjWZCRjM8iuCMobb38v6QkMzgub5ZB72trsOEAEBZCYoLekRjGkYxdWm7p4xEpwAcuScXXZB6cwiuCLPpCDNu5H7X0ZBfZBYoG75eZC6LJPLO3rzSupnNZBTwVBcZAPqta9VGxsxuCvQZDZD'; // Replace with your Facebook access token
+      
+      const response = await axios.get(apiUrl, {
+>>>>>>> Stashed changes
         params: {
           fields: fields,
           access_token: accessToken
@@ -34,6 +69,7 @@ async function getAdsets () {
     } catch(error) {
       console.error('Error fetching data:', error);
     }
+<<<<<<< Updated upstream
 }
   
 async function adsetEsists(adsetId){
@@ -111,3 +147,99 @@ async function main() {
 }
 
 main()
+=======
+  }
+
+
+
+
+
+
+
+
+  async function populateCampaigns(facebookCampaignData) {
+    try {
+      const query = `
+        INSERT INTO fb_campaigns 
+          (campaign_id, status, created_time, daily_budget, objective, start_time, stop_time, buying_type, budget_remaining, 
+          bid_strategy, primary_attribution, source_campaign, special_ad_categories, updated_time, name, ad_account_id, omni_business_id, db_updated_at) 
+        VALUES 
+          ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)
+        ON CONFLICT (campaign_id) DO UPDATE SET 
+          status = EXCLUDED.status,
+          created_time = EXCLUDED.created_time,
+          daily_budget = EXCLUDED.daily_budget,
+          objective = EXCLUDED.objective,
+          start_time = EXCLUDED.start_time,
+          stop_time = EXCLUDED.stop_time,
+          buying_type = EXCLUDED.buying_type,
+          budget_remaining = EXCLUDED.budget_remaining,
+          bid_strategy = EXCLUDED.bid_strategy,
+          primary_attribution = EXCLUDED.primary_attribution,
+          source_campaign = EXCLUDED.source_campaign,
+          special_ad_categories = EXCLUDED.special_ad_categories,
+          updated_time = EXCLUDED.updated_time,
+          name = EXCLUDED.name,
+          ad_account_id = EXCLUDED.ad_account_id,
+          omni_business_id = EXCLUDED.omni_business_id,
+          db_updated_at = EXCLUDED.db_updated_at;
+        `;
+      const values = [
+        facebookCampaignData.campaign_id, facebookCampaignData.status, facebookCampaignData.created_time, facebookCampaignData.daily_budget, 
+        facebookCampaignData.objective, facebookCampaignData.start_time, facebookCampaignData.stop_time, facebookCampaignData.buying_type, 
+        facebookCampaignData.budget_remaining, facebookCampaignData.bid_strategy, facebookCampaignData.primary_attribution, facebookCampaignData.source_campaign, 
+        facebookCampaignData.special_ad_categories, facebookCampaignData.updated_time, facebookCampaignData.name, facebookCampaignData.ad_account_id, 
+        facebookCampaignData.omni_business_id, facebookCampaignData.db_updated_at
+      ];
+  
+      const result = await client.query(query, values);
+      console.log(`Inserted or updated campaign: ${facebookCampaignData.campaign_id} successfully`);
+    } catch (err) {
+      console.error('Insert or update error:', err.stack);
+    } finally {
+      // Close the client connection
+      //client.end();
+    }
+  };
+  
+
+
+
+
+
+async function main () {
+  const facebookCampaignData = await getCampaigns()
+
+  //console.log(facebookCampaignData)
+
+  for (let i = 0; i < facebookCampaignData.campaigns.data.length; i++) {
+    const campaignData = {
+      campaign_id: facebookCampaignData.campaigns.data[i].id,
+      status: facebookCampaignData.campaigns.data[i].status,
+      created_time: facebookCampaignData.campaigns.data[i].created_time,
+      daily_budget: facebookCampaignData.campaigns.data[i].daily_budget / 100 || '',
+      objective: facebookCampaignData.campaigns.data[i].objective,
+      start_time: facebookCampaignData.campaigns.data[i].start_time,
+      stop_time: facebookCampaignData.campaigns.data[i].stop_time,
+      buying_type: facebookCampaignData.campaigns.data[i].buying_type,
+      budget_remaining: facebookCampaignData.campaigns.data[i].budget_remaining / 100 || '',
+      bid_strategy: facebookCampaignData.campaigns.data[i].bid_strategy || '',
+      primary_attribution: facebookCampaignData.campaigns.data[i].primary_attribution,
+      source_campaign: facebookCampaignData.campaigns.data[i].source_campaign || '',
+      special_ad_categories: facebookCampaignData.campaigns.data[i].special_ad_categories,
+      updated_time: facebookCampaignData.campaigns.data[i].updated_time,
+      name: facebookCampaignData.campaigns.data[i].name, 
+      ad_account_id: facebookCampaignData.id,
+      omni_business_id: 'b_zfPwbkxKMDfeO1s9fn5TejRILh34hd',
+      db_updated_at: new Date(),
+    }
+    populateCampaigns(campaignData);
+  
+  }
+}
+
+
+main();
+
+
+>>>>>>> Stashed changes
